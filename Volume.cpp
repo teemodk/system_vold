CyanogenMod/android_system_vold

Volume: Don't crap out when dealing with unformatted cards
And plug a memory leak when formatting them

Change-Id: I2ab58328d07a343a5ec9e1c4b58f8258f0c9133c

Volume.cpp
@@ -253,7 +253,7 @@ int Volume::createDeviceNode(const char *path, int major, int minor) {
 
 int Volume::formatVol(bool wipe, const char* fstype) {
 
-    const char* fstype2 = NULL;
+    char* fstype2 = NULL;
 
     if (getState() == Volume::State_NoMedia) {
         errno = ENODEV;
@@ -307,11 +307,18 @@ int Volume::formatVol(bool wipe, const char* fstype) {
     if (fstype == NULL) {
         fstype2 = getFsType((const char*)devicePath);
     } else {
-        fstype2 = fstype;
+        fstype2 = strdup(fstype);
     }
 
     if (mDebug) {
         SLOGI("Formatting volume %s (%s) as %s", getLabel(), devicePath, fstype2);
+    }
+
+    /* If the device has no filesystem, let's default to vfat.
+     * A NULL fstype2 will cause a MAPERR in the format
+     * switch below */
+    if (fstype2 == NULL) {
+        fstype2 = strdup("vfat");
     }
 
     if (strcmp(fstype2, "exfat") == 0) {
@@ -327,6 +334,8 @@ int Volume::formatVol(bool wipe, const char* fstype) {
     if (ret < 0) {
         SLOGE("Failed to format (%s)", strerror(errno));
     }
+
+    free(fstype2);
 
 err:
     setState(Volume::State_Idle);

teemodk/system_vold

Volume.cpp
@@ -298,6 +298,13 @@ int Volume::formatVol(bool wipe) {
 
     fstype = getFsType((const char*)devicePath);
 
+    /* If the device has no filesystem, let's default to vfat.
+     * A NULL fstype will cause a MAPERR in the format
+     * switch below */
+    if (fstype == NULL) {
+        fstype = strdup("vfat");
+    }
+
     if (mDebug) {
         SLOGI("Formatting volume %s (%s) as %s", getLabel(), devicePath, fstype);
     }